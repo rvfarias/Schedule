69a8ecd04cefb150742b62b207ec30b72b8d1146
Rafael <rafaelvlfarias@gmail.com>
2025-10-09 14:13:29 -0300

    Lógica de edição de pessoa cadastrada

--- backend/app/routers/people.py
+++ backend/app/routers/people.py
@@ -1,19 +1,19 @@
 from fastapi  import APIRouter, Depends
 from sqlalchemy.orm import Session
 from app.core.database import get_db
-from app.models import person as models
+from app.models import person
 from app.models.availability import Availability
-from app.schemas import person_schema as schemas
+from app.schemas import person_schema
 
 router = APIRouter(prefix="/people", tags=["people"])
 
-@router.post("/", response_model=schemas.PersonResponse)
-def create_person(person: schemas.PersonCreate, db: Session = Depends(get_db)):
+@router.post("/", response_model=person_schema.PersonResponse)
+def create_person(person: person_schema.PersonCreate, db: Session = Depends(get_db)):
     avaliability_objs =[
         Availability(**a.dict()) for a in person.availability
     ]
 
-    new_person = models.Person(
+    new_person = person.Person(
         name=person.name,
         last_name=person.last_name,
         availability=avaliability_objs
@@ -23,6 +23,21 @@ def create_person(person: schemas.PersonCreate, db: Session = Depends(get_db)):
     db.refresh(new_person)
     return new_person
 
-@router.get("/", response_model=list[schemas.PersonResponse])
+@router.put("/{person_id}", response_model=person_schema.PersonResponse)
+def update_person(person_id: int, new_person: person_schema.PersonResponse, db: Session = Depends(get_db)):
+    db_person = db.query(person.Person).filter(person.Person.id == person_id).first()
+    
+    if not db_person:
+        return None
+    
+    db_person.name = new_person.name
+    db_person.last_name = new_person.last_name
+    db_person.availability = [Availability(**a.dict()) for a in new_person.availability]
+    db.commit()
+    db.refresh(db_person)
+    return db_person 
+
+
+@router.get("/", response_model=list[person_schema.PersonResponse])
 def list_people(db: Session = Depends(get_db)):
-    return db.query(models.Person).all()
+    return db.query(person.Person).all()
