dfd3d1fe50680dbae878999f7920edf423f519d2
Rafael <rafaelvlfarias@gmail.com>
2025-10-09 19:30:53 -0300

    Alterando tipo da dado de turno para JSON

--- backend/app/models/availability.py
+++ backend/app/models/availability.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy import Column, Integer, ForeignKey, JSON
 from sqlalchemy.orm import relationship
 from app.core.database import Base
 
@@ -7,6 +7,6 @@ class Availability(Base):
 
     id = Column(Integer, primary_key=True, index=True)
     day = Column(Integer, nullable=False)  # 0=Sunday, 1=Monday, ..., 6=Saturday
-    period = Column(String, nullable=False)  # e.g., "morning", "afternoon", "evening"
+    period = Column(JSON, nullable=False)  # e.g., "morning", "afternoon", "evening"
     person_id = Column(Integer, ForeignKey("people.id"), nullable=False)
     person = relationship("Person", back_populates="availability")
--- backend/app/schemas/availability_schema.py
+++ backend/app/schemas/availability_schema.py
@@ -1,8 +1,9 @@
 from pydantic import BaseModel
+from typing import List
 
 class Availability(BaseModel):
     day: int  # 0=Sunday, 1=Monday, ..., 6=Saturday
-    period: str  # e.g., "morning", "afternoon", "evening"
+    period: List[str]  # e.g., "morning", "afternoon", "evening"
 
 class AvailabilityCreate(Availability):
     pass
