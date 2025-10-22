c8303207d001c188c972b028f753a1a0cde8c6be
Rafael <rafaelvlfarias@gmail.com>
2025-10-22 19:48:54 -0300

    Ajustes para melhorar consistência dos dados

--- backend/app/models/schedule.py
+++ backend/app/models/schedule.py
@@ -10,5 +10,5 @@ class Schedule(Base):
     year = Column(Integer, nullable=False)
     max_period_per_person = Column(Integer, nullable=False)
     assignments = Column(JSON) # Armazena as atribuições como um campo JSON
-    people = relationship("Person", back_populates="schedule")
+    people = relationship("Person", back_populates="schedule", cascade="all, delete-orphan")
     days = relationship("ScheduleDay", back_populates="schedule", cascade="all, delete-orphan")
